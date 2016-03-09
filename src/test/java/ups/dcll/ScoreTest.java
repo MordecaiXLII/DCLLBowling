28a5090b3d0a148983188181357a2c256ed4f930
MordecaiXLII <mordecaixlii@gmail.com>
2016-03-09 14:38:48 +0100

    Les tests OMG oui !!!111!!

--- src/test/java/ups/dcll/ScoreTest.java
+++ src/test/java/ups/dcll/ScoreTest.java
@@ -11,6 +11,10 @@ public class ScoreTest {
 
     @Test
     public void testScore() throws Exception {
+        assertEquals(Score.score(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}), 0);
+        assertEquals(Score.score(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}), 300);
+        assertEquals(Score.score(new int[]{9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9}), 190);
+        assertEquals(Score.score(new int[]{1, 2, 3, 4, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10, 0, 10, 6}), 156);
 
     }
 }
