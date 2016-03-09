6729f475df5159062951f193ad06e5fbea07e5f4
Alexandre Lagane <minizarbi@hotmail.fr>
2016-03-09 14:20:09 +0100

    Added ScoreTest and removed default classes

---
+++ src/test/java/ups/dcll/ScoreTest.java
@@ -0,0 +1,16 @@
+package ups.dcll;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alexandre Lagane on 3/9/16.
+ */
+public class ScoreTest {
+
+    @Test
+    public void testScore() throws Exception {
+
+    }
+}
