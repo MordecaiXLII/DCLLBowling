11f2912dd7cd5bd5cd5f960d742cb752400b3ac9
MordecaiXLII <mordecaixlii@gmail.com>
2016-03-09 15:02:22 +0100

    correction tests assertEquals(res, val) et pas (val, res)

--- src/test/java/ups/dcll/ScoreTest.java
+++ src/test/java/ups/dcll/ScoreTest.java
@@ -11,10 +11,10 @@ public class ScoreTest {
 
     @Test
     public void testScore() throws Exception {
-        assertEquals(Score.score(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}), 0);
-        assertEquals(Score.score(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}), 300);
-        assertEquals(Score.score(new int[]{9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9}), 190);
-        assertEquals(Score.score(new int[]{1, 2, 3, 4, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10, 0, 10, 6, 3}), 145);
+        assertEquals(0, Score.score(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
+        assertEquals(300, Score.score(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}));
+        assertEquals(190, Score.score(new int[]{9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9}));
+        assertEquals(145, Score.score(new int[]{1, 2, 3, 4, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10, 0, 10, 6, 3}));
 
     }
 }
