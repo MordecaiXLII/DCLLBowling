6a9cbe8d455bdc9152bda87caddd68b690c8cdc5
Alexandre Lagane <minizarbi@hotmail.fr>
2016-03-09 15:22:23 +0100

    Completed isValid

--- src/main/java/ups/dcll/Score.java
+++ src/main/java/ups/dcll/Score.java
@@ -4,21 +4,26 @@ package ups.dcll;
  * Created by Alexandre Lagane on 3/2/16.
  */
 public abstract class Score {
-    public static int score(int tab[]) {
+    private static final int strikeScore = 10;
+    private static final int spareScore = 10;
+    private static final int framesNumber = 10;
+    private static final int pinsNumber = 10;
+
+    public static int score(int[] tab) {
         int total = 0;
         int frame = 0;
         int i = 0;
         int frameScore = 0;
-        while (frame < 10) {
-            if (tab[i] == 10) { /* Strike */
-                total += 10 + tab[i + 1] + tab[i + 2];
+        while (frame < framesNumber) {
+            if (tab[i] == pinsNumber) { /* Strike */
+                total += strikeScore + tab[i + 1] + tab[i + 2];
             } else {
                 frameScore = tab[i];
                 i++;
-                if (frameScore + tab[i] == 10) { /* Spare */
-                    total += 10 + tab[i + 1];
+                if (frameScore + tab[i] == pinsNumber) { /* Spare */
+                    total += spareScore + tab[i + 1];
                 } else { /* Miss */
-                    total = frameScore + tab[i];
+                    total += frameScore + tab[i];
                 }
             }
             i++;
@@ -31,24 +36,34 @@ public abstract class Score {
     public static boolean isValid(int tab[]) {
         int test;
         int frame = 0;
-        int i = 0;
+        int i;
         int frameScore = 0;
+
+        for (i = 0; i < tab.length; i++) {
+            if (tab[i] < 0 || tab[i] > pinsNumber)
+                return true;
+        }
+
+        i = 0;
         try {
-            while (frame < 10) {
-                if (tab[i] == 10) { /* Strike */
+            while (frame < framesNumber) {
+                if (tab[i] == pinsNumber) { /* Strike */
                     test = tab[i + 1];
                     test = tab[i + 2];
                 } else {
                     frameScore = tab[i];
                     i++;
-                    if (frameScore + tab[i] == 10) { /* Spare */
+                    if (frameScore + tab[i] > pinsNumber)
+                        return false;
+                    if (frameScore + tab[i] == pinsNumber) { /* Spare */
                         test = tab[i + 1];
-                    } else { /* Miss */ }
+                    }
+                    /* else Miss */
                 }
                 i++;
                 frame++;
             }
-        } catch (ArrayIndexOutOfBoundsException) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
 
